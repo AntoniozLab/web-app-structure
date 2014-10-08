3224536a5625ec059f01aa713c3c307718719ce0
Juan Cahuana <jcahuanam@gmail.com>
2014-10-08 16:01:07 -0500

    eliminamos el uso de variables innecesarias, eliminamos el usode nib, agregamos el plugin size

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,20 +1,21 @@
-var nib = require('nib'), //Agrega prefijos de CSS de manera automática
-	gulp = require('gulp'), //Gulp
-	notify = require("gulp-notify"), //Notificar en el computador
-	jade = require('gulp-jade'), //Compilar Jade
-	prefix = require('gulp-autoprefixer'), //Autoprefixer de CSS
-	stylus = require('gulp-stylus'), //Compilar stylus
-	changed = require('gulp-changed'), //Verificar si el archivo ha cambiando para aplicarle o no un pipe
-	image = require('gulp-image'), //Optimiza imágenes PNG, JPEG, GIF y SVG, pero no lo usaremos para archivos PNG, a cambio usaremos tinypng
-	imagemin = require('gulp-tinypng'),//Optimiza archivos PNG con mayor eficiencia, pero se necesita de un token. Para más info visitar -> https://tinypng.com
-	watch = require('gulp-watch'), //Mira cambios en los archivos que uno indique y lanza una acción, por ejemplo recargar el navegador.
-	uglify = require('gulp-uglify'), //Optimizar archivos javascript
-	plumber = require('gulp-plumber'), //Seguir ejecutando los pipe de gulp aunque exista un error
-	connect = require('gulp-connect'), //Servidor node con livereload
-	concat = require('gulp-concat'), //Concatenar
-	stripDebug = require('gulp-strip-debug'), //Eliminar comentarios y comandos como console.log
-	argv = require('yargs').argv; //Permite pasar argumentos vía consola del sistema
+'use strict';
 
+var gulp = require('gulp'); //Gulp
+var notify = require('gulp-notify'); //Notificar en el computador
+var jade = require('gulp-jade'); //Compilar Jade
+var prefix = require('gulp-autoprefixer'); //Autoprefixer de CSS
+var stylus = require('gulp-stylus'); //Compilar stylus
+var changed = require('gulp-changed'); //Verificar si el archivo ha cambiando para aplicarle o no un pipe
+var image = require('gulp-image'); //Optimiza imágenes PNG, JPEG, GIF y SVG, pero no lo usaremos para archivos PNG, a cambio usaremos tinypng
+var imagemin = require('gulp-tinypng');//Optimiza archivos PNG con mayor eficiencia, pero se necesita de un token. Para más info visitar -> https://tinypng.com
+var watch = require('gulp-watch'); //Mira cambios en los archivos que uno indique y lanza una acción, por ejemplo recargar el navegador.
+var uglify = require('gulp-uglify'); //Optimizar archivos javascript
+var plumber = require('gulp-plumber'); //Seguir ejecutando los pipe de gulp aunque exista un error
+var connect = require('gulp-connect'); //Servidor node con livereload
+var concat = require('gulp-concat'); //Concatenar
+var stripDebug = require('gulp-strip-debug'); //Eliminar comentarios y comandos como console.log
+var size = require('gulp-size');
+var argv = require('yargs').argv; //Permite pasar argumentos vía consola del sistema
 
 /* ===========================================================================
 RUTAS DE DESARROLLO Y PRODUCCIÓN
@@ -52,20 +53,20 @@ gulp.task('html', function(){
 		gulp.src('./jade/*.jade')
 			.pipe(plumber())
 			.pipe(jade())
+      .pipe(size({title: 'HTML Optimizados:'}))
 			.pipe(gulp.dest(prodPath))
-			.pipe(notify("HTML Producción: <%= file.relative %>!"));
+			.pipe(notify('HTML Producción: <%= file.relative %>!'));
 
 	}else{
 	//Desarrollo
 
-		var watchInit = false;
 		gulp.src('./jade/*.jade')
 			.pipe(plumber())
 			.pipe(watch({ emit:'one' }))
 			.pipe(jade({ pretty: true }))
 			.pipe(gulp.dest(devPath))
 			.pipe(connect.reload())
-			.pipe(notify("HTML Desarrollo: <%= file.relative %>!"));
+			.pipe(notify('HTML Desarrollo: <%= file.relative %>!'));
 
 		gulp.src('./jade/includes/*.jade')
 			.pipe(watch({ emit:'one' }, function(){
@@ -75,13 +76,12 @@ gulp.task('html', function(){
 					.pipe(jade({ pretty: true }))
 					.pipe(gulp.dest(devPath))
 					.pipe(connect.reload())
-					.pipe(notify("HTML Desarrollo by Jade Includes: <%= file.relative %>!"));
+					.pipe(notify('HTML Desarrollo by Jade Includes: <%= file.relative %>!'));
 
 			}));
 	}
 
 });
-
 
 /* ===========================================================================
 CSS
@@ -95,12 +95,13 @@ gulp.task('css', function(){
 		destPath = prodPath + 'css/';
 		gulp.src('./stylus/*.min.styl')
 			.pipe(plumber({
-				errorHandler: notify.onError("Error: <%= error.message %>")
+				errorHandler: notify.onError('Error: <%= error.message %>')
 			}))
 			.pipe(stylus({compress: true}))
-			.pipe(prefix("last 1 version", "> 1%", "ie 8", "ie 7"))
+			.pipe(prefix('last 1 version', '> 1%', 'ie 8', 'ie 7'))
+      .pipe(size({title: 'CSS Optimizados:'}))
 			.pipe(gulp.dest(destPath))
-			.pipe(notify("CSS Producción: <%= file.relative %>!"));
+			.pipe(notify('CSS Producción: <%= file.relative %>!'));
 
 	}else{
 	//Desarrollo
@@ -109,13 +110,13 @@ gulp.task('css', function(){
 		watch({glob: './stylus/*.styl'}, function(){
 			gulp.src('./stylus/*.min.styl')
 				.pipe(plumber({
-					errorHandler: notify.onError("Error: <%= error.message %>")
+					errorHandler: notify.onError('Error: <%= error.message %>')
 				}))
 				.pipe(stylus())
-				.pipe(prefix("last 1 version", "> 1%", "ie 8", "ie 7"))
+				.pipe(prefix('last 1 version', '> 1%', 'ie 8', 'ie 7'))
 				.pipe(gulp.dest(destPath))
 				.pipe(connect.reload())
-				.pipe(notify("CSS Desarrollo: <%= file.relative %>!"));
+				.pipe(notify('CSS Desarrollo: <%= file.relative %>!'));
 		});
 	}
 });
@@ -141,7 +142,7 @@ gulp.task('js', function () {
 			.pipe(changed(destPath + 'vendor/'))
 			.pipe(stripDebug())
 			.pipe(gulp.dest(destPath + 'vendor/'))
-			.pipe(notify("Html5shiv.js & Respond.js Producción: <%= file.relative %>!"));
+			.pipe(notify('Html5shiv.js & Respond.js Producción: <%= file.relative %>!'));
 
 		//Página Home
 		gulp.src(filesHome)
@@ -149,8 +150,9 @@ gulp.task('js', function () {
 			.pipe(uglify())
 			//.pipe(uglify().on('error', function(e) { console.log('\x07',e.message); return this.end(); })) //--> Muestra un mensaje más completo del error
 			.pipe(stripDebug())
+      .pipe(size({title: 'JS Optimizados:'}))
 			.pipe(gulp.dest(destPath))
-			.pipe(notify("JS Producción: <%= file.relative %>!"));
+			.pipe(notify('JS Producción: <%= file.relative %>!'));
 
 	}else{
 	//Desarrollo
@@ -163,7 +165,7 @@ gulp.task('js', function () {
 					.pipe(concat('home.min.js'))
 					.pipe(gulp.dest(destPath))
 					.pipe(connect.reload())
-					.pipe(notify("JS Desarrollo: <%= file.relative %>!"));
+					.pipe(notify('JS Desarrollo: <%= file.relative %>!'));
 			}));
 	}
 
@@ -176,7 +178,11 @@ gulp.task('images', function(){
 	var destPath = prodPath + 'img/';
 	gulp.src([devPath+'img/*', '!'+devPath+'img/*.png'])
 		.pipe(changed(destPath))
-		.pipe(image())
+		.pipe(image({
+      progressive: true,
+      interlaced: true
+    }))
+    .pipe(size({title: 'JPG Optimizados:'}))
 		.pipe(gulp.dest(destPath));
 });
 
@@ -190,7 +196,8 @@ gulp.task('png', function(){
 
 	gulp.src(devPath + 'img/*.png')
 		.pipe(changed(destPath))
-		.pipe(imagemin("CHD9zVb-3FcqW3C0kzIX_fR3L-UArybO"))
+		.pipe(imagemin('CHD9zVb-3FcqW3C0kzIX_fR3L-UArybO'))
+    .pipe(size({title: 'PNG Optimizados:'}))
 		.pipe(gulp.dest(destPath));
 });
 
