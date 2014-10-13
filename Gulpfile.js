7f6234c17e1117dfb48080e5472d39f3902d452d
Juan Cahuana <jcahuanam@gmail.com>
2014-10-13 00:31:59 -0500

    Cambiamos la estructura de carpetas en app y public

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,36 +1,37 @@
 'use strict';
 
-var gulp = require('gulp'); //Gulp
-var notify = require('gulp-notify'); //Notificar en el computador
-var jade = require('gulp-jade'); //Compilar Jade
-var prefix = require('gulp-autoprefixer'); //Autoprefixer de CSS
-var stylus = require('gulp-stylus'); //Compilar stylus
-var changed = require('gulp-changed'); //Verificar si el archivo ha cambiando para aplicarle o no un pipe
-var image = require('gulp-image'); //Optimiza imágenes PNG, JPEG, GIF y SVG, pero no lo usaremos para archivos PNG, a cambio usaremos tinypng
-var imagemin = require('gulp-tinypng');//Optimiza archivos PNG con mayor eficiencia, pero se necesita de un token. Para más info visitar -> https://tinypng.com
-var watch = require('gulp-watch'); //Mira cambios en los archivos que uno indique y lanza una acción, por ejemplo recargar el navegador.
-var uglify = require('gulp-uglify'); //Optimizar archivos javascript
-var plumber = require('gulp-plumber'); //Seguir ejecutando los pipe de gulp aunque exista un error
-var connect = require('gulp-connect'); //Servidor node con livereload
-var concat = require('gulp-concat'); //Concatenar
+var gulp       = require('gulp'); //Gulp
+var notify     = require('gulp-notify'); //Notificar en el computador
+var jade       = require('gulp-jade'); //Compilar Jade
+var prefix     = require('gulp-autoprefixer'); //Autoprefixer de CSS
+var stylus     = require('gulp-stylus'); //Compilar stylus
+var changed    = require('gulp-changed'); //Verificar si el archivo ha cambiando para aplicarle o no un pipe
+var image      = require('gulp-image'); //Optimiza imágenes PNG, JPEG, GIF y SVG, pero no lo usaremos para archivos PNG, a cambio usaremos tinypng
+var imagemin   = require('gulp-tinypng');//Optimiza archivos PNG con mayor eficiencia, pero se necesita de un token. Para más info visitar -> https://tinypng.com
+var watch      = require('gulp-watch'); //Mira cambios en los archivos que uno indique y lanza una acción, por ejemplo recargar el navegador.
+var uglify     = require('gulp-uglify'); //Optimizar archivos javascript
+var plumber    = require('gulp-plumber'); //Seguir ejecutando los pipe de gulp aunque exista un error
+var connect    = require('gulp-connect'); //Servidor node con livereload
+var concat     = require('gulp-concat'); //Concatenar
 var stripDebug = require('gulp-strip-debug'); //Eliminar comentarios y comandos como console.log
-var size = require('gulp-size');
-var argv = require('yargs').argv; //Permite pasar argumentos vía consola del sistema
+var size       = require('gulp-size');
+var argv       = require('yargs').argv; //Permite pasar argumentos vía consola del sistema
 
 /* ===========================================================================
 RUTAS DE DESARROLLO Y PRODUCCIÓN
 =========================================================================== */
-var devPath = './app/frontend/';
-var prodPath = './public/frontend/';
+var devPath = './app/';
+var prodPath = './public/';
 
 /* ===========================================================================
 SERVIDOR DE DESARROLLO
 =========================================================================== */
 gulp.task('server-dev', function() {
   connect.server({
-    root: 'app/frontend/',
+    root: 'app/',
     livereload: true
   });
+  gulp.src('').pipe(notify('Servidor de Desarrollo Listo'));
 });
 
 /* ===========================================================================
@@ -38,8 +39,9 @@ SERVIDOR DE PRODUCCIÓN
 =========================================================================== */
 gulp.task('server-prod', function () {
 	connect.server({
-		root: 'public/frontend/'
+		root: 'public/'
 	});
+  gulp.src('').pipe(notify('Servidor de Producción Listo'));
 });
 
 /* ===========================================================================
@@ -99,7 +101,7 @@ gulp.task('css', function(){
 			}))
 			.pipe(stylus({compress: true}))
 			.pipe(prefix('last 1 version', '> 1%', 'ie 8', 'ie 7'))
-      .pipe(size({title: 'CSS Optimizados:'}))
+      .pipe(size({title: 'Tamaño de CSS Optimizados:'}))
 			.pipe(gulp.dest(destPath))
 			.pipe(notify('CSS Producción: <%= file.relative %>!'));
 
@@ -130,7 +132,7 @@ gulp.task('js', function () {
 	// Archivos a concatenar para la página del home
 	var filesHome = [
 		'./bower_components/jquery/dist/jquery.js',
-		'./app/frontend/js/home.js'
+		'./app/js/home.js'
 	];
 
 	if(argv.production){
@@ -138,11 +140,11 @@ gulp.task('js', function () {
 	destPath = prodPath + 'js/';
 
 		//Copia html5shiv.js y respond.js a producción
-		gulp.src(devPath + 'js/vendor/html5-3.6-respond-1.1.0.min.js')
+		gulp.src(devPath + 'js/vendor/*.js')
 			.pipe(changed(destPath + 'vendor/'))
 			.pipe(stripDebug())
 			.pipe(gulp.dest(destPath + 'vendor/'))
-			.pipe(notify('Html5shiv.js & Respond.js Producción: <%= file.relative %>!'));
+			.pipe(notify('JS Vendor a Producción: <%= file.relative %>!'));
 
 		//Página Home
 		gulp.src(filesHome)
@@ -150,16 +152,16 @@ gulp.task('js', function () {
 			.pipe(uglify())
 			//.pipe(uglify().on('error', function(e) { console.log('\x07',e.message); return this.end(); })) //--> Muestra un mensaje más completo del error
 			.pipe(stripDebug())
-      .pipe(size({title: 'JS Optimizados:'}))
+      .pipe(size({title: 'Tamaño de JS Optimizados:'}))
 			.pipe(gulp.dest(destPath))
-			.pipe(notify('JS Producción: <%= file.relative %>!'));
+			.pipe(notify('JS a Producción: <%= file.relative %>!'));
 
 	}else{
 	//Desarrollo
 	destPath = devPath + 'js/';
 
 		//Página Home
-		gulp.src(['./app/frontend/js/home.js'])
+		gulp.src(['./app/js/home.js'])
 			.pipe(watch({ emit: 'all' }, function(){
 				gulp.src(filesHome)
 					.pipe(concat('home.min.js'))
